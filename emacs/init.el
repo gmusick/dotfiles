681ac07b3f08f4b2e9fcc11aebc646b224ef3800
Greg Musick <greg.musick@gmail.com>
2015-06-23 21:24:52 -0700

    Add emacs config

---
+++ emacs/init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+(load-theme 'monokai t)  ;; theme
+(global-linum-mode t)  ;; set line numbers
+(set-frame-font "inconsolata-g 13")  ;; font
+
+;; hide toolbar
+(if window-system
+  (tool-bar-mode -1))
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
